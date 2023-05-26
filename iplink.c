a81cfa92bb5514b674cea722b6edcdbcec016895
backchristoffer <cback@redhat.com>
2023-05-26 15:47:52 +0200

    another crash test

--- iplink.c
+++ iplink.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {   
     int r;
-    int i = 0;
     r=system("ip -s link");
-    while (i <= 5) {
-        printf("%d\n",r);
-        ++i;
+    while (1) {
+        return 3;
     }
     return 0;
 }
