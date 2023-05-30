982c7e34bbee0d303793b6f9702c3056bfcb2043
backchristoffer <cback@redhat.com>
2023-05-30 21:51:24 +0200

    timestamp test

--- iplink.c
+++ iplink.c
@@ -12,14 +12,8 @@ static void sig_handler(int _)
     keep_running = 0;
 }
 
-int timestamp()
-{
-    int st=system("date");
-    return st;
-}
-
 int iplink() {
-    int r=system("ip -s link");   
+    int r=system("date & ip -s link");   
     return r;
 }
 
@@ -28,7 +22,6 @@ int main(void)
     signal(SIGINT, sig_handler);
     
     while (keep_running) {
-        timestamp();
         iplink();
         sleep(30);
     }
