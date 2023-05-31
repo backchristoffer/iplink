90faf0daaaa95d33dcffd92fd686df00fd77d6d3
backchristoffer <cback@redhat.com>
2023-05-31 12:07:01 +0200

    added thread and timestamp

--- iplink.c
+++ iplink.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <pthread.h>
+#include <time.h>
 
 static volatile sig_atomic_t keep_running = 1;
 
@@ -11,18 +13,29 @@ static void sig_handler(int _)
     keep_running = 0;
 }
 
-int iplink() {
+void* timestamp( void * arg) {
+    time_t t;
+    time(&t);
+    printf("%s",ctime(&t));
+    return NULL;
+}
+
+void* iplink( void * arg) {
     int r=system("ip -s link");   
-    return r;
+    return NULL;
 }
 
 int main(void)
 {   
+    pthread_t linkthread;
+    pthread_t tsthread;
     signal(SIGINT, sig_handler);
-    
+
     while (keep_running) {
-        iplink();
+        pthread_create(&linkthread, NULL, timestamp, NULL);
+        pthread_create(&tsthread, NULL, iplink, NULL);
         sleep(30);
     }
+
     return EXIT_SUCCESS;
 }
