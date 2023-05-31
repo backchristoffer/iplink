6575ae741f94e430331e8487dd51af88d3e005d5
backchristoffer <cback@redhat.com>
2023-05-31 06:59:20 +0200

    timestamp() removed

--- iplink.c
+++ iplink.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <time.h>
 #include <signal.h>
 
 static volatile sig_atomic_t keep_running = 1;
@@ -10,12 +9,6 @@ static void sig_handler(int _)
 {
     (void)_;
     keep_running = 0;
-}
-
-int timestamp()
-{
-    int st=system("date");
-    return st;
 }
 
 int iplink() {
@@ -28,7 +21,6 @@ int main(void)
     signal(SIGINT, sig_handler);
     
     while (keep_running) {
-        timestamp();
         iplink();
         sleep(30);
     }
